2c91ec715f4baa79496156c2d2de2039e19401e4
Vladimir Burlakov <vvb0x56@gmail.com>
2019-07-10 12:17:43 +0300

    Update cow.sh

--- cow.sh
+++ cow.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
-AFORISM=`curl -s --connect-timeout 1  -X POST -d "method=getQuote&format=text&lang=ru" http://api.forismatic.com/api/1.0/`
+#AFORISM=`curl -s --connect-timeout 1  -X POST -d "method=getQuote&format=text&lang=ru" http://api.forismatic.com/api/1.0/`
+
+AFORISM=`/Users/mac/go/src/aforism/main`
+
 SHUF_BIN="shuf"
 COWS_DIR="/usr/share/cowsay/cows"
 
-if [ "$(uname)" == "Darwin" ]; then 
+if [ "$(uname)" == "Darwin" ]; then
     SHUF_BIN="gshuf"
     COWS_DIR="/usr/local/Cellar/cowsay/3.03/share/cows"
 fi
 
-if [ -z "$AFORISM" ]; then 
+if [ -z "$AFORISM" ]; then
     AFORISM=`$SHUF_BIN -n 1 ~/Documents/aforism.md`
 fi
 
